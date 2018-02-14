d83d304648613199b44d689d03c58b3a06c2a4e2
Dilantha Haputhanthri <hdilantha95@gmail.com>
2018-02-14 23:39:28 +0530

    Initial commit
    Back-end functionality - Adding a user to the database

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
+
+// User Schema
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String
+    },
+    password: {
+        type: String,
+        required:true
+    },
+    type: {
+        type: String
+    },
+    telephone: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true
+    }
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = function(id, callback) {
+    User.findById(id, callback);
+}
+
+module.exports.getUserByUserName = function(username, callback) {
+    const query = {username: username}
+    User.findOne(query, callback);
+}
+
+module.exports.addUser = function(newUser, callback) {
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(newUser.password, salt, function(err, hash) {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+}
---
+++ routes/users.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
+// Register
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name: req.body.name,
+        password: req.body.password,
+        type: req.body.type,
+        telephone: req.body.telephone,
+        email: req.body.email
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if(err) {
+            res.json({success: false, msg:'Failed to register user'});
+        } else {
+            res.json({success: true, msg:'User registered'});
+        }
+    });
+});
+
+// Authenticate
+router.get('/authenticate', (req, res, next) => {
+    res.send('AUTHENTICATE');
+});
+
+// Profile
+router.get('/profile', (req, res, next) => {
+    res.send('PROFILE');
+});
+
+module.exports = router;
